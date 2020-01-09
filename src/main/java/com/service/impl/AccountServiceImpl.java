c9e020f47f4549c9c17734ae27f94f6dd12a67bf
qianyuhebb <824608693.@qq.com>
2020-01-09 23:29:11 +0800

    Initial commit

---
+++ src/main/java/com/service/impl/AccountServiceImpl.java
@@ -0,0 +1,22 @@
+package com.service.impl;
+
+import com.dao.AccountDao;
+import com.factory.BeanFactory;
+import com.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+
+//    accountDaoImpl accountDao = new accountDaoImpl();
+      /* int i =1;*/
+
+    public void saveAccout() {
+
+
+
+        System.out.println("SERVICE 保存成功");
+
+
+
+
+    }
+}
